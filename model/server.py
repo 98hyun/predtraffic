fb860e5fa343b3fac1c52e225b889a1474dbda33
98hyun <98hyun@kakao.com>
2021-12-15 15:22:37 +0900

    20211215 마지막

--- model/server.py
+++ model/server.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import datetime 
 import pandas as pd
 import numpy as np
+np.random.seed(71)
 import re
 
 import matplotlib.pyplot as plt
@@ -13,6 +14,9 @@ rc('font', family=font)
 from sklearn.preprocessing import LabelEncoder
 import requests
 from bs4 import BeautifulSoup
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def daysbin(x):
     if x<10:
@@ -85,10 +89,30 @@ traindata.drop('사고유형대분류',axis=1,inplace=True)
 
 traindata['지역'] = le.fit_transform(traindata['지역'])
 
-from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import RandomOverSampler
+rand_over_sample = RandomOverSampler(sampling_strategy='auto', random_state=41)
+x_over, y_over = rand_over_sample.fit_resample(traindata, target)
 
-model = RandomForestClassifier(n_estimators=10)
-model.fit(traindata, target)
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(x_over,y_over, test_size=0.2, random_state=71)
+# print(X_train.shape,y_train.shape)
+# print("-------")
+# print(X_test.shape,y_test.shape)
+
+# from sklearn.ensemble import RandomForestClassifier
+# model = RandomForestClassifier(n_estimators=10)
+
+from xgboost import XGBClassifier
+xgb_wrapper = XGBClassifier(n_estimators=400, learning_rate=0.1, max_depth=3)
+evals = [(X_test, y_test)]
+xgb_wrapper.fit(X_train, y_train,early_stopping_rounds=10, eval_metric="merror", 
+                eval_set=evals)
+
+# model = RandomForestClassifier(n_estimators=10)
+# model.fit(traindata, target)
+
+from sklearn.metrics import plot_confusion_matrix,accuracy_score
 
 def main():
     today=datetime.today()
@@ -116,7 +140,7 @@ def main():
             ## {month}월 어떤 사고가 많이 일어나는지.
             df=train.groupby(['월','사고유형대분류'])['사고유형대분류'].count()
             top_acc=df.loc[month].idxmax()
-            count=df.loc[month][top_acc]
+            count=df.loc[month].loc[top_acc]
             kind={1:'차대사람',2:'차대차',3:'차량단독'}
             st.markdown(f"<h4 style='text-align:center;color:red'>{kind[top_acc]} <span style='color:rgb(49, 51, 63);'>{count}회</span></h4>",unsafe_allow_html=True)
         with kpi2:
@@ -129,9 +153,10 @@ def main():
             ## 11월 몇시에 사고가 가장 많이 나는지.
             df=train.groupby(['월','시간'])['사고유형대분류'].count()
             top_acc_time=df.loc[month].idxmax()
-            count=df.loc[month][top_acc]
-            st.markdown(f"<h4 style='text-align:center;color:red'>{kind[top_accj]} <span style='color:rgb(49, 51, 63);'>과 {top_acc_time}시</span></h4>",unsafe_allow_html=True)
-            st.markdown(f"<span style='font-size:4px;'>*24시기준</span>",unsafe_allow_html=True)
+            count=df.loc[month][top_acc_time]
+            st.markdown(f"<h4 style='text-align:center;color:red'>{kind[top_accj]} <span style='color:rgb(49, 51, 63);'>과 {top_acc_time}시</span><br><span style='font-size:4px;color:black;'>*24시기준</span></h4>",unsafe_allow_html=True)
+            # st.markdown(f"<span style='font-size:4px;'>*24시기준</span>",unsafe_allow_html=True)
+
         kpi3,kpi4,kpi5=st.columns(3)
         with kpi3:
             ## 11월 무슨요일에 사고가 가장 많이 나는지
@@ -140,19 +165,19 @@ def main():
             top_acc_time=df.loc[month].idxmax()
             days={1:'일',2:'월',3:'화',4:'수',5:'목',6:'금',7:'토'}
             st.markdown("<h3 style='text-align:center'>최다사고요일</h3>",unsafe_allow_html=True)
-            st.markdown(f"<h4 style='text-align:center;color:red'>{days[top_acc]}요일</h4>",unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align:center;color:red'>{days[top_acc_time]}요일</h4>",unsafe_allow_html=True)
         with kpi4:
             ## 11월 어떤 지역에서 사고가 가장 많이 나는지.
             df=train.groupby(['월','지역'])['사고유형대분류'].count()
             top_acc=df.loc[month].idxmax()
-            count=df.loc[month].loc[top_acc]
+            count=df.loc[month][top_acc]
             st.markdown("<h3 style='text-align:center'>최다사고지역</h3>",unsafe_allow_html=True)
             st.markdown(f"<h4 style='text-align:center;color:red;'>{top_acc} <span style='color:rgb(49, 51, 63);'>{count}회</span></h4>",unsafe_allow_html=True)
         with kpi5:
             ## 11월 초,중,말에서 사고가 언제 가장 많이 나는지.
             df=train.groupby(['월','일_bin'])['사고유형대분류'].count()
             top_acc=df.loc[month].idxmax()
-            count=df.loc[month].loc[top_acc]
+            count=df.loc[month][top_acc]
             days_bin={0:'초',1:'중',2:'말'}
             st.markdown("<h3 style='text-align:center'>최다사고날짜</h3>",unsafe_allow_html=True)
             st.markdown(f"<h4 style='text-align:center;color:rgb(49, 51, 63);'>{month}월 <span style='color:red;'>{days_bin[top_acc]}</span></h4>",unsafe_allow_html=True)
@@ -160,6 +185,18 @@ def main():
 
         st.markdown(f"<h3>사고다발구역과 법규위반구역</h3>",unsafe_allow_html=True)
         st.map(mapdata)
+
+        st.write(" \n ")
+        st.write(" \n ")
+        st.write(" \n ")
+        st.write(" \n ")
+        st.write(" \n ")
+        st.write(" \n ")
+
+        st.markdown(f"<center style='font-size:10px;font-weight:bold;'>사용 데이터</center>",unsafe_allow_html=True)
+        st.markdown(f"<center style='font-size:10px;'>도로교통공단_스쿨존어린이사고다발지역정보서비스</center>",unsafe_allow_html=True)
+        st.markdown(f"<center style='font-size:10px;'>도로교통공단_사망교통사고정보서비스</center>",unsafe_allow_html=True)
+        st.markdown(f"<center style='font-size:10px;'>전국어린이보호구역표준데이터</center>",unsafe_allow_html=True)
     else:
         dong=st.selectbox('지역 선택',[i for i in le.classes_])
         url=f'https://www.google.com/search?q={dong}+날씨'
@@ -186,17 +223,32 @@ def main():
             juya=2
         conv_dong=le.transform([dong]).item()
 
-        pred=model.predict_proba([[month,day,hour,temp,wind,rain,hum,juya,weekday,conv_dong]])
+        pred_data=pd.DataFrame({'월':[month],'일':[day],'시간':[hour],'기온(°C)':[temp],'풍속(m/s)':[wind],'강수량(mm)':[rain],'습도(%)':[hum],'주야구분':[juya],'요일':[weekday],'지역':[conv_dong]})
+        # st.dataframe(pred_data)
+        pred=xgb_wrapper.predict_proba(pred_data)
         kpi1,kpi2,kpi3=st.columns(3)
+        print(round(pred[0][0],1))
         with kpi1:
             st.markdown(f"<h2 style='color:red;text-align:center'>차대차</h2>",unsafe_allow_html=True)
-            st.markdown(f"<h3>사고확률 : {pred[0][0]*100}%</h3>",unsafe_allow_html=True)
+            st.markdown(f"<h3>사고확률 : {round(pred[0][0]*100,1)}%</h3>",unsafe_allow_html=True)
         with kpi2:
             st.markdown(f"<h2 style='color:red;text-align:center'>차대사람</h2>",unsafe_allow_html=True)
-            st.markdown(f"<h3>사고확률 : {pred[0][1]*100}%</h3>",unsafe_allow_html=True)
+            st.markdown(f"<h3>사고확률 : {round(pred[0][1]*100,1)}%</h3>",unsafe_allow_html=True)
         with kpi3:
             st.markdown(f"<h2 style='color:red;text-align:center'>차량단독</h2>",unsafe_allow_html=True)
-            st.markdown(f"<h3>사고확률 : {pred[0][2]*100}%</h3>",unsafe_allow_html=True)
+            st.markdown(f"<h3>사고확률 : {round(pred[0][2]*100,1)}%</h3>",unsafe_allow_html=True)
+
+        # model_conp = RandomForestClassifier(n_estimators=100)
+        # model_conp.fit(X_train,y_train)
+
+        pred=xgb_wrapper.predict(traindata)
+
+        # st.markdown(f"<h3><center>confusion matrix accuracy score : <span style='color:red;'>{round(accuracy_score(target,pred)*100,1)}%</span></center></h3>",unsafe_allow_html=True)
+
+        fig,ax=plt.subplots(1,1,figsize=(8,6),dpi=140)
+        plot_confusion_matrix(xgb_wrapper,traindata,target,ax=ax)
+        st.pyplot(fig)
+        
         
 if __name__=='__main__':
     main()
